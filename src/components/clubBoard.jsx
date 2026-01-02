04f36b6e5da8f3d34b2ca9b9e7bf52e84ec7171d
RonenFran <99738216+RonenFran@users.noreply.github.com>
2026-01-02 11:28:53 -0400

    Create join button in clubBoard.jsx

--- src/components/clubBoard.jsx
+++ src/components/clubBoard.jsx
@@ -1,10 +1,17 @@
 const ClubBoard = () => {
   return (
     <div className="mx-auto h-[150vh] w-2/3 border-[16px] border-primary rounded-xl">
-      <h2 className="text-8xl font-bold text-primary text-center mt-40">
+      <div className="relative flex w-48 h-16 px-4 rounded-md justify-center items-center justify-self-end bg-primary-light text-secondary font-bold top-8 right-8">
+        <span className="flex-1 text-center text-xl">Join Now</span>
+        <svg viewBox="0 0 10 10" fill="white" className="size-12">
+          <rect width="7" height="1.5" x="1.5" y="4.25" rx="0.5" />
+          <rect width="1.5" height="7" x="4.25" y="1.5" ry="0.5" />
+        </svg>
+      </div>
+      <h2 className="text-6xl font-bold text-primary text-center mt-8">
         Message Board
       </h2>
-      <div className="mx-auto h-screen w-2/3 border-[16px] border-primary rounded-xl mt-20"></div>
+      <div className="mx-auto h-screen w-2/3 border-[16px] border-primary rounded-xl mt-8"></div>
     </div>
   );
 };
