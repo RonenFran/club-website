0f5dbb30d6e3defd5241ca887731424bdbbcfa91
RonenFran <99738216+RonenFran@users.noreply.github.com>
2026-02-21 19:08:08 -0400

    Add login for opening and closing profile/login menu

--- client/src/components/profileDropdown.jsx
+++ client/src/components/profileDropdown.jsx
@@ -1,27 +1,35 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function ProfileDropdown() {
   const { isAuthenticated } = useAuth();
-  const [displayDropdown, setDisplayDropdown] = useState("hidden");
+  const [displayDropdown, setDisplayDropdown] = useState(false);
+  const dropdownRef = useRef(null);
 
-  useEffect(() => {}, [
-    {
-      /* hover */
-    },
-  ]);
+  useEffect(() => {
+    function pointerDownEvent(e) {
+      if (!dropdownRef.current) return;
+      if (dropdownRef.current.contains(e.target)) return;
+      setDisplayDropdown(false);
+    }
 
-  useEffect(
-    () => {},
-    [
-      /* click */
-    ]
-  );
+    if (displayDropdown) {
+      document.addEventListener("pointerdown", pointerDownEvent);
+    }
+
+    return () => {
+      document.removeEventListener("pointerdown", pointerDownEvent);
+    };
+  }, [displayDropdown]);
 
   return (
-    <div>
-      <div className="bg-gray-400 size-12 p-2 rounded-3xl mr-6">
+    <div ref={dropdownRef}>
+      <div
+        className="bg-gray-400 size-12 p-2 rounded-3xl mr-4 cursor-pointer"
+        id="profile"
+        onClick={() => setDisplayDropdown(!displayDropdown)}
+      >
         <svg viewBox="0 0 5 5" fill="white" aria-hidden="true" className="size-8">
           <circle r="1" cx="2.5" cy="1.5" />
           <circle r="1.7" cx="2.5" cy="4.5" />
@@ -30,7 +38,7 @@ export default function ProfileDropdown() {
 
       {isAuthenticated ? (
         <div
-          className={`flex flex-col absolute top-20 right-2 w-24 text-center bg-secondary shadow-md rounded-md px-1 z-10 text-lg font-semibold border-[1px] text-primary ${displayDropdown}`}
+          className={`flex flex-col absolute top-20 right-2 w-24 text-center bg-secondary shadow-md rounded-md px-1 z-10 text-lg font-semibold border-[1px] text-primary ${displayDropdown ? "" : "hidden"}`}
         >
           <Link to="/profile" className="py-1 border-b-2">
             Profile
@@ -41,9 +49,9 @@ export default function ProfileDropdown() {
         </div>
       ) : (
         <div
-          className={`flex flex-col absolute top-20 right-2 w-24 text-center bg-secondary shadow-md rounded-md px-1 z-10 text-lg font-semibold border-[1px] text-primary ${displayDropdown}`}
+          className={`flex flex-col absolute top-20 right-2 w-36 text-center bg-secondary shadow-md rounded-md px-1 z-10 text-xl font-semibold border-[1px] text-primary ${displayDropdown ? "" : "hidden"}`}
         >
-          <div className="absolute -top-2 right-8 size-4 bg-secondary rotate-45 border-t-[1px] border-l-[1px] -z-10 " />
+          <div className="absolute -top-2 right-6 size-4 bg-secondary rotate-45 border-t-[1px] border-l-[1px] -z-10" />
 
           <Link to="/login" className="py-1 border-b-2">
             Login
