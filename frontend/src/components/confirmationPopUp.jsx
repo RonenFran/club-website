034856b0603b94b6e006a516900ea0974fce0081
RonenFran <99738216+RonenFran@users.noreply.github.com>
2026-05-04 21:33:50 -0300

    Create API call for removing club membership and begin building confirmation pop up

---
+++ frontend/src/components/confirmationPopUp.jsx
@@ -0,0 +1,7 @@
+export default function confirmationPopUp({ message, confirm, cancel }) {
+  return (
+    <div className="absolute width-96 h-60 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div></div>
+    </div>
+  );
+}
