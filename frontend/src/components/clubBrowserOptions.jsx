64b619e46408065b257dffc26cefb7ffc859bd0b
RonenFran <99738216+RonenFran@users.noreply.github.com>
2026-05-30 22:07:41 -0300

    Chagne search bar styling in browser

--- frontend/src/components/clubBrowserOptions.jsx
+++ frontend/src/components/clubBrowserOptions.jsx
@@ -3,32 +3,32 @@ export default function ClubBrowserOptions({ tagSetter }) {
     <div className="z-30 sticky top-0 w-32 h-screen bg-gray-800">
       <div className="flex flex-col gap-4 h-full justify-center font-bold text-lg text-secondary-200">
         <div
-          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-red-800 transition-all duration-200 ease-in-out hover:w-48"
+          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-red-800 transition-all duration-200 ease-in-out hover:w-40"
           // style={{ writingMode: "sideways-lr" }} Sideways writing which is bad design, but feels like it kind of fits
           // Will return to this later
         >
           All
         </div>
         <div
-          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-yellow-800 transition-all duration-200 ease-in-out hover:w-48"
+          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-yellow-800 transition-all duration-200 ease-in-out hover:w-40"
           // style={{ writingMode: "sideways-lr" }}
         >
           Sports
         </div>
         <div
-          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-green-800 transition-all duration-200 ease-in-out hover:w-48"
+          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-green-800 transition-all duration-200 ease-in-out hover:w-40"
           // style={{ writingMode: "sideways-lr" }}
         >
           Societies
         </div>
         <div
-          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-blue-800 transition-all duration-200 ease-in-out hover:w-48"
+          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-blue-800 transition-all duration-200 ease-in-out hover:w-40"
           // style={{ writingMode: "sideways-lr" }}
         >
           Social
         </div>
         <div
-          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-purple-800 transition-all duration-200 ease-in-out hover:w-48"
+          className="flex items-end justify-center p-8 border-secondary border-3 border-l-0 w-28 h-fit rounded-r-xl bg-purple-800 transition-all duration-200 ease-in-out hover:w-40"
           // style={{ writingMode: "sideways-lr" }}
         >
           Research
--- frontend/src/components/searchBar.jsx
+++ frontend/src/components/searchBar.jsx
@@ -3,8 +3,8 @@ import { GoSearch } from "react-icons/go";
 export default function SearchBar({ searchSetter }) {
   return (
     <div className="z-30 sticky top-0 w-full h-20 bg-primary-500 p-4">
-      <div className="flex gap-2 items-center bg-primary-400/50 w-98/100 h-12 p-3 mb-4 text-secondary mx-auto rounded-2xl">
-        <GoSearch size={24} className="box-content border-r-1 pr-2" />
+      <div className="flex gap-2 items-center bg-primary-400/50 w-98/100 h-12 mb-4 text-secondary mx-auto rounded-2xl">
+        <GoSearch size={24} className="box-content border-r-1 px-2" />
         <input
           type="text"
           id="search"
@@ -14,6 +14,7 @@ export default function SearchBar({ searchSetter }) {
             console.log(e.target.value);
             searchSetter(e.target.value);
           }}
+          className="h-full w-full focus:outline-0"
         />
       </div>
     </div>
